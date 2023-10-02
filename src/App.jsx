f6828149e217f473882bdad29867ef26c3fa85d9
Rohin Bharatee <rohinbharatee@gmail.com>
2023-10-02 20:53:36 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Routes from './Routes';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Routes />
+    </>
+  )
+}
+
+export default App
---
+++ src/Routes.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Routes as Switch, Route } from 'react-router-dom';
+
+const Routes = () => {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default Routes
